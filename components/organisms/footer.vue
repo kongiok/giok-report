e51a3040d0b77037b01df1b0d62b5cc4480ae104
kongiok <marxw.me@gmail.com>
2024-07-25 14:06:13 +0800

    feat: Add Footer & Header

---
+++ components/organisms/footer.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <footer>
+    <!-- FAB Card -->
+    <div class="fab card">
+    
+    </div>
+    <!-- Contacts -->
+    
+  </footer>
+</template>
+
+<style scoped></style>
