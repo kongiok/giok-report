2bed1a9f02c6064604d3e0ad30063b7442cfc8d3
kongiok <marxw.me@gmail.com>
2024-08-02 15:54:04 +0800

    feat: customizing link with props & styles

---
+++ components/atoms/link.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title: string;
+  to: string;
+  descrip?: string;
+  icon?:string;
+}>();
+</script>
+
+<template>
+  <nuxt-link-locale
+  :to="to"
+  :aria-details="descrip"
+  class="link"
+  >
+    <p class="text">{{ title }}</p>
+    <Icon v-show="icon" class="icon" :name="icon" />
+  </nuxt-link-locale>
+</template>
+
+<style scoped>
+.link {
+  @apply inline-flex flex-row gap-2 items-center text-center;
+  @apply text-concrete-800 active:text-olive-drab-600;
+}
+.text {
+  @apply text-lg capitalize;
+}
+.icon {
+  @apply text-xl;
+}
+</style>
