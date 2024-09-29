88cb70b9e330280dc7545f326e2c37febb87a65b
kongiok <211410146@365.tku.edu.tw>
2024-09-29 15:39:11 +0800

    feat(i18n): add localization structure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,45 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
+  modules: [
+    "@nuxt/content",
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/i18n",
+  ],
 
   routeRules: {
-    '/': { prerender: true }
+    "/": { prerender: true },
+  },
+  i18n: {
+    langDir: "locales/",
+    locales: [
+      {
+        code: "en-US",
+        iso: "en-US",
+        name: "English",
+        files: ["en-US/common.json", "en-US/nav.json"],
+      },
+      {
+        code: "zh-TW",
+        iso: "zh-TW",
+        name: "繁體中文",
+        files: ["zh-TW/common.json", "zh-TW/nav.json"],
+      },
+      {
+        code: "min-TW",
+        iso: "min-TW",
+        name: "台語",
+        files: ["min-TW/common.json", "min-TW/nav.json"],
+      },
+    ],
+    defaultLocale: "zh-TW",
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: "i18n_redirected",
+      alwaysRedirect: true,
+      redirectOn: "root",
+    },
   },
 
-  compatibilityDate: '2024-09-19'
-})
+  compatibilityDate: "2024-09-19",
+});
