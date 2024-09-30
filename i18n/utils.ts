fda42bda907fcabf5680a2a24154b4feeb1228d3
kongiok <211410146@365.tku.edu.tw>
2024-09-30 15:53:22 +0800

    refactor(i18n): suit localization up
    In previous version, we discovered that locales are not that clearify for internalization.
    Therefore, we put locales directory into i18n, pull out configs about different languages into utils directory make it easier to use.

---
+++ i18n/utils.ts
@@ -0,0 +1,38 @@
+export const localesSet = [
+  {
+    code: "en-US",
+    language: "en-US",
+    name: "English",
+    files: [
+      "en-US/common.json",
+      "en-US/nav.json",
+      "en-US/error.json",
+      "en-US/docs.json",
+    ],
+  },
+  {
+    code: "zh-TW",
+    language: "zh-TW",
+    name: "繁體中文",
+    files: [
+      "zh-TW/common.json",
+      "zh-TW/nav.json",
+      "zh-TW/error.json",
+      "zh-TW/docs.json",
+    ],
+  },
+  {
+    code: "min-TW",
+    language: "min-TW",
+    name: "台語",
+    files: [
+      "min-TW/common.json",
+      "min-TW/nav.json",
+      "min-TW/error.json",
+      "min-TW/docs.json",
+    ],
+  },
+];
+
+export const localeDefault = "zh-TW";
+export const langDir = "i18n/locales/";
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,8 @@
+import {
+  localesSet as locales,
+  localeDefault as defaultLocale,
+  langDir,
+} from "./i18n/utils";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
@@ -12,43 +17,9 @@ export default defineNuxtConfig({
   },
   i18n: {
     strategy: "prefix",
-    langDir: "locales/",
-    defaultLocale: "zh-TW",
-    locales: [
-      {
-        code: "en-US",
-        language: "en-US",
-        name: "English",
-        files: [
-          "en-US/common.json",
-          "en-US/nav.json",
-          "en-US/error.json",
-          "en-US/docs.json",
-        ],
-      },
-      {
-        code: "zh-TW",
-        language: "zh-TW",
-        name: "繁體中文",
-        files: [
-          "zh-TW/common.json",
-          "zh-TW/nav.json",
-          "zh-TW/error.json",
-          "zh-TW/docs.json",
-        ],
-      },
-      {
-        code: "min-TW",
-        language: "min-TW",
-        name: "台語",
-        files: [
-          "min-TW/common.json",
-          "min-TW/nav.json",
-          "min-TW/error.json",
-          "min-TW/docs.json",
-        ],
-      },
-    ],
+    locales,
+    defaultLocale,
+    langDir,
     detectBrowserLanguage: {
       useCookie: true,
       cookieKey: "i18n_redirected",
